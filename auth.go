273c20e5808ba21a56048b13d3ea94f6cacfedff
acoshift <acoshift@gmail.com>
2017-01-31 15:04:30 +0700

    add token invalid error

--- auth.go
+++ auth.go
@@ -59,41 +59,41 @@ func (auth *Auth) CreateCustomToken(userID string, claims interface{}) (string, 
 func (auth *Auth) VerifyIDToken(idToken string) (*Claims, error) {
 	token, err := jwt.ParseWithClaims(idToken, &Claims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, fmt.Errorf("firebaseauth: Firebase ID token has incorrect algorithm. Expected \"RSA\" but got \"%#v\"", token.Header["alg"])
+			return nil, &ErrTokenInvalid{fmt.Sprintf("firebaseauth: Firebase ID token has incorrect algorithm. Expected \"RSA\" but got \"%#v\"", token.Header["alg"])}
 		}
 		kid := token.Header["kid"].(string)
 		if kid == "" {
-			return nil, fmt.Errorf("firebaseauth: Firebase ID token has no \"kid\" claim")
+			return nil, &ErrTokenInvalid{"firebaseauth: Firebase ID token has no \"kid\" claim"}
 		}
 		key := auth.selectKey(kid)
 		if key == nil {
-			return nil, fmt.Errorf("firebaseauth: Firebase ID token has \"kid\" claim which does not correspond to a known public key. Most likely the ID token is expired, so get a fresh token from your client app and try again")
+			return nil, &ErrTokenInvalid{"firebaseauth: Firebase ID token has \"kid\" claim which does not correspond to a known public key. Most likely the ID token is expired, so get a fresh token from your client app and try again"}
 		}
 		return key, nil
 	})
 	if err != nil {
-		return nil, err
+		return nil, &ErrTokenInvalid{err.Error()}
 	}
 
-	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
-		if !claims.verifyAudience(auth.app.projectID) {
-			return nil, fmt.Errorf("firebaseauth: Firebase ID token has incorrect \"aud\" (audience) claim. Expected \"%s\" but got \"%s\"", auth.app.projectID, claims.Audience)
-		}
-		if !claims.verifyIssuer("https://securetoken.google.com/" + auth.app.projectID) {
-			return nil, fmt.Errorf("firebaseauth: Firebase ID token has incorrect \"iss\" (issuer) claim. Expected \"https://securetoken.google.com/%s\" but got \"%s\"", auth.app.projectID, claims.Issuer)
-		}
-		if claims.Subject == "" {
-			return nil, fmt.Errorf("firebaseauth: Firebase ID token has an empty string \"sub\" (subject) claim")
-		}
-		if len(claims.Subject) > 128 {
-			return nil, fmt.Errorf("firebaseauth: Firebase ID token has \"sub\" (subject) claim longer than 128 characters")
-		}
-
-		claims.UserID = claims.Subject
-
-		return claims, nil
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, &ErrTokenInvalid{"firebaseauth: invalid token"}
 	}
-	return nil, fmt.Errorf("firebaseauth: invalid token")
+	if !claims.verifyAudience(auth.app.projectID) {
+		return nil, &ErrTokenInvalid{fmt.Sprintf("firebaseauth: Firebase ID token has incorrect \"aud\" (audience) claim. Expected \"%s\" but got \"%s\"", auth.app.projectID, claims.Audience)}
+	}
+	if !claims.verifyIssuer("https://securetoken.google.com/" + auth.app.projectID) {
+		return nil, &ErrTokenInvalid{fmt.Sprintf("firebaseauth: Firebase ID token has incorrect \"iss\" (issuer) claim. Expected \"https://securetoken.google.com/%s\" but got \"%s\"", auth.app.projectID, claims.Issuer)}
+	}
+	if claims.Subject == "" {
+		return nil, &ErrTokenInvalid{"firebaseauth: Firebase ID token has an empty string \"sub\" (subject) claim"}
+	}
+	if len(claims.Subject) > 128 {
+		return nil, &ErrTokenInvalid{"firebaseauth: Firebase ID token has \"sub\" (subject) claim longer than 128 characters"}
+	}
+
+	claims.UserID = claims.Subject
+	return claims, nil
 }
 
 func (auth *Auth) fetchKeys() error {
--- error.go
+++ error.go
@@ -9,3 +9,13 @@ var (
 	ErrRequireServiceAccount = errors.New("firebase: requires service account")
 	ErrRequireUID            = errors.New("firebaseauth: require user id")
 )
+
+// ErrTokenInvalid is the invalid token error
+type ErrTokenInvalid struct {
+	s string
+}
+
+// Error implements error interface
+func (err *ErrTokenInvalid) Error() string {
+	return err.s
+}
