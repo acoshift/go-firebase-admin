60f6812e66bf0c3c223e31560a1f81061cc24b39
acoshift <acoshift@gmail.com>
2017-02-19 02:06:56 +0700

    database: implement RefFromURL

--- database.go
+++ database.go
@@ -1,8 +1,10 @@
 package admin
 
 import (
+	"fmt"
 	"net"
 	"net/http"
+	_url "net/url"
 	_path "path"
 	"time"
 )
@@ -41,4 +43,16 @@ func newDatabase(app *App) *Database {
 func (database *Database) Ref(path string) *Reference {
 	path = _path.Clean(path)
 	return &Reference{database: database, path: path}
+}
+
+// RefFromURL returns a Reference from an url
+func (database *Database) RefFromURL(url string) (*Reference, error) {
+	u, err := _url.Parse(url)
+	if err != nil {
+		return nil, err
+	}
+	if u.Host != database.app.databaseURL {
+		return nil, fmt.Errorf("firebasedatabase: invalid host %v", u.Host)
+	}
+	return database.Ref(u.Path), nil
 }
