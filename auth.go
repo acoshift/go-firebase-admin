e2722d4ba091f7078002d9be8951d7e2d852d695
acoshift <acoshift@gmail.com>
2017-07-10 09:49:44 +0700

    auth: remove unused error check

--- auth.go
+++ auth.go
@@ -213,9 +213,6 @@ func (auth *Auth) createUserAutoID(ctx context.Context, user *User) (string, err
 	if err != nil {
 		return "", err
 	}
-	if len(resp.LocalId) == 0 {
-		return "", errors.New("firebaseauth: create account error")
-	}
 	return resp.LocalId, nil
 }
 
