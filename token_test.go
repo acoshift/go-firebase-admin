74f5fe6fe7c07353d5d3245fff7cf0bb76e211c5
acoshift <acoshift@gmail.com>
2017-09-02 21:50:42 +0700

    add leeway

--- token_test.go
+++ token_test.go
@@ -1,49 +1,41 @@
-package firebase_test
+package firebase
 
 import (
 	"testing"
 	"time"
 
-	"fmt"
-
-	firebase "github.com/acoshift/go-firebase-admin"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestValidToken(t *testing.T) {
 	t.Run("Valid", func(t *testing.T) {
-		now := time.Now()
-		token := &firebase.Token{
-			IssuedAt:  now.Unix(),
-			ExpiresAt: now.Unix(),
-		}
+		token := &Token{}
 
 		err := token.Valid()
-		assert.Nil(t, err)
+		assert.NoError(t, err)
 	})
 
-	t.Run("usedbefore", func(t *testing.T) {
-		now := time.Now().AddDate(1, 0, 0)
-		now2 := time.Now()
-		token := &firebase.Token{
-			IssuedAt:  now.Unix(),
-			ExpiresAt: now2.Unix(),
+	t.Run("verifyIssuedAt", func(t *testing.T) {
+		now := time.Now()
+		token := &Token{
+			IssuedAt: now.Unix(),
 		}
 
-		err := token.Valid()
-		assert.NotNil(t, err)
-		assert.Equal(t, fmt.Errorf("token used before issued"), err)
+		res := token.verifyIssuedAt(now.Unix())
+		assert.True(t, res)
+
+		res = token.verifyIssuedAt(now.Unix() + 60 /* leeway */)
+		assert.True(t, res)
 	})
 
-	t.Run("expired", func(t *testing.T) {
+	t.Run("verifyExpiresAt", func(t *testing.T) {
 		now := time.Now()
-		token := &firebase.Token{
+		token := &Token{
 			IssuedAt:  now.Unix(),
 			ExpiresAt: 1500651130,
 		}
 
-		err := token.Valid()
-		assert.NotNil(t, err)
-		assert.Equal(t, fmt.Errorf("token is expired by %v", time.Unix(now.Unix(), 0).Sub(time.Unix(token.ExpiresAt, 0))), err)
+		res := token.verifyExpiresAt(now.Unix())
+		assert.False(t, res)
 	})
 }
